21dfdc5250fefaf01fb1bc613dec0bbeb23b7903
mitchellciupak <ciupak@purdue.edu>
2022-04-25 15:35:51 -0500

    onenote integration functions init

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,25 @@
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
+
+// OneNote Integration
+
+// Create Page
+export const createOneNotePage = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+// Create Section
+export const createOneNoteSection = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase!");
+});
+
+// Add to Page
+export const updateOneNotePage = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase!");
+});
+
