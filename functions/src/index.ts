b5fa456fd10c83ef0f6d6481a28cd0ebc928fc2e
mitchellciupak <ciupak@purdue.edu>
2022-04-27 09:20:01 -0500

    adding main and notion initial funtions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,16 @@ import * as functions from "firebase-functions";
 
 // // https://firebase.google.com/docs/functions/typescript
 
+// Master
+export const nlog = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase!");
+});
+
+export const nlog_finish = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase!");
+});
 
 // OneNote Integration
 
@@ -23,3 +33,14 @@ export const updateOneNotePage = functions.https.onRequest((request, response) =
   response.send("Hello from Firebase!");
 });
 
+
+// Notion Integration
+export const createNotionMediaEntry = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase!");
+});
+
+export const createNotionTrainingLogEntry = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase!");
+});
